d9859598350615a58d317c1e7446279e50892e42
root <root@moc-integration-test-python-x8fvm>
2023-12-13 22:17:00 +0000

    Removing comment for sync

--- test_model.py
+++ test_model.py
@@ -8,7 +8,6 @@ import math
 print("Starting program", flush=True)
 
 # add comments below
-# comment 2023-12-13 22:16:37.526551
 # comment 2023-12-12 22:17:45.981612
 # comment 2023-12-11 17:48:38.071997
 
