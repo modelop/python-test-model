636a81743f3df29efc34971f2238fe2160411540
root <root@moc-integration-test-python-wbn69>
2023-12-15 22:16:47 +0000

    Removing comment for sync

--- test_model.py
+++ test_model.py
@@ -8,7 +8,6 @@ import math
 print("Starting program", flush=True)
 
 # add comments below
-# comment 2023-12-15 22:16:35.154503
 # comment 2023-12-14 22:17:43.180281
 # comment 2023-12-13 22:17:23.759129
 # comment 2023-12-12 22:17:45.981612
